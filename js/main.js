15519c649cc0f08eb9e3e63a0656a10b42bfa3d3
AgustinErcoli <ercoliagustin@gmail.com>
2021-09-14 19:27:41 -0300

    agregando js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let nombreDelUsuario = prompt("Ingrese su nombre");
+console.log(nombreDelUsuario);
+let ingresado = "Hola" + " " + nombreDelUsuario + "!"
+alert(ingresado);
+let mailDelUsuario = prompt("Ingrese su mail");
+console.log(mailDelUsuario);
+let ingresado2 = mailDelUsuario + " " + "ha sido ingresado"
+alert(ingresado2);
+const anio_Actual = 2021;
+let anioNacimiento = prompt("ingrese su año de nacimiento")
+let edadActual = anio_Actual - anioNacimiento;
+let ingresado3 =   "Tienes" + " " + edadActual + " " + "años"
+alert(ingresado3)
