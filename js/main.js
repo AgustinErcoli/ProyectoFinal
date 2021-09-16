2d43ed150d251997b98db2b1c0272625611d533c
AgustinErcoli <ercoliagustin@gmail.com>
2021-09-16 20:54:57 -0300

    2 proyecto js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-let nombreDelUsuario = prompt("Ingrese su nombre");
-console.log(nombreDelUsuario);
-let ingresado = "Hola" + " " + nombreDelUsuario + "!"
-alert(ingresado);
-let mailDelUsuario = prompt("Ingrese su mail");
-console.log(mailDelUsuario);
-let ingresado2 = mailDelUsuario + " " + "ha sido ingresado"
-alert(ingresado2);
-const anio_Actual = 2021;
-let anioNacimiento = prompt("ingrese su año de nacimiento")
-let edadActual = anio_Actual - anioNacimiento;
-let ingresado3 =   "Tienes" + " " + edadActual + " " + "años"
-alert(ingresado3)
+let nombreDeComida = prompt("Que vas a querer comer?");
+
+if ((nombreDeComida == "Niguiris") || (nombreDeComida == "niguiris") || (nombreDeComida == "NIGUIRIS")) {
+    alert("Es una buena eleccion si te gusta lo simple");
+} else if ((nombreDeComida == "Rolls Clasicos") || (nombreDeComida == "Rolls clasicos") || (nombreDeComida == "ROLLS CLASICOS") || (nombreDeComida == "rolls clasicos")) { 
+    alert("Es una buena opcion si te gusta lo clasico");
+} else if ((nombreDeComida == "Rolls Vegetarianos") || (nombreDeComida == "Rolls vegetarianos") || (nombreDeComida == "ROLLS VEGETARIANOS") || (nombreDeComida == "rolls vegetarianos")) {
+    alert("Es una buena elecion si eres vegetariano/a");
+} else if ((nombreDeComida == "Rolls Veganos") || (nombreDeComida == "Rolls veganos") || (nombreDeComida == "ROLLS VEGANOS") || (nombreDeComida == "rolls veganos")) {
+    alert("Es una buena eleccion si eres vegano/a");
+    console.log(nombreDeComida + " no tienen nigun ingrediente proveniente de animales!")
+} else {
+    alert("No tenemos ningun platillo llamado " +nombreDeComida+ ". Debrias revisar nuestro menu nuevamente.")
+}
