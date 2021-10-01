967f7b9a4440d8b7acdc246b35766bcd53e1fbc1
AgustinErcoli <ercoliagustin@gmail.com>
2021-10-01 18:22:31 -0300

    arrays y preentrega

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
-let producto = prompt("Ingrese el producto");
-let precio = prompt("Ingrese el precio del producto");
-let cantidad = prompt("Ingrese la cantidad deseada");
+const TASAFINANCIACION = 0.05;
 
-class productos{
-    constructor(producto, precio, cantidad) {
-        this.nombre = producto;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-    guardar(){
-    console.log("El producto es: " + this.nombre); 
-    console.log("El precio es: $" + this.precio);
-    console.log("La cantidad es: " + this.cantidad);
-    console.log("El total es: $" + (this.precio * this.cantidad)); 
+let totalCompra = parseInt(prompt("Ingresar valor total de la compra a financiar"))
+let cantidadCuotas = parseInt(prompt("Elija la cantidad de cuotas de financiación"))
+
+const sumar = (a,b) => a + b;
+const restar = (a,b) => a - b;
+const multiplicar = (a,b) => a * b;
+const dividir = (a,b) => a / b;
+const potencia = (a,b) => a ** b;
+
+let cuotaMensual = dividir(multiplicar(totalCompra,TASAFINANCIACION),restar(1,potencia(sumar(1,TASAFINANCIACION),-cantidadCuotas)));
+
+alert(`Usted va a pagar ${cantidadCuotas} cuotas de ${Math.round(cuotaMensual)} pesos cada una`);
+
+class Capueat{
+constructor (Nombre, Precio, Cantidad){
+this.Nombre = Nombre;
+this.Precio = Precio;
+this.Cantidad = Cantidad;
     }
 }
-const producto1 = new productos(producto, precio, cantidad);
-producto1.guardar();
+
+let activoCapueat1 = [];
+
+activoCapueat1.push (new Capueat ("Niguiris", "$175", 4, 1));
+activoCapueat1.push (new Capueat ("Rolls Especiales", "$530", 8, 1));
+activoCapueat1.push (new Capueat ("Rolls Veganos", "$430", 8, 1));
+activoCapueat1.push (new Capueat ("Rolls calientes vegetarianos", "$485", 8, 1));
+
+console.log(activoCapueat1);
